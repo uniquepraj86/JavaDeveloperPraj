6e8b005a453ccd34a76d6e02d78439db78797db9
uniqpraj86 <prajarote86@gmail.com>
2022-07-22 16:50:13 +0530

    Praj-AirthmeticOperation

---
+++ src/AirthmaticOperation.java
@@ -0,0 +1,16 @@
+public class AirthmaticOperation {
+    public static void main(String[] args) {
+        int x = 53;
+        int y = 82;
+        int result;
+        //Addition
+        result = x+y;
+        System.out.println("Addition is :"+result);
+        result = x-y;
+        System.out.println("Substraction is :"+result);
+        result = x*y;
+        System.out.println("Multipivation is :"+result);
+        result = x/y;
+        System.out.println("Division is :"+result);
+    }
+}
