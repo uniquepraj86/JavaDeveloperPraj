d0ef58c02780061e9aa37cefab8d087e383c0ac0
uniqpraj86 <prajarote86@gmail.com>
2022-07-22 21:35:08 +0530

    Praj-if else

---
+++ src/basic/IfElse.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        if (a>b) {
+            System.out.println("a gretest number");
+        }
+        else
+        {
+            System.out.println("b is Gretest number");
+        }
+    }
+}
