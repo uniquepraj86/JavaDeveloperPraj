e43f82cd84596df48ca550be8a50fe7ef5d1ee0a
uniqpraj86 <prajarote86@gmail.com>
2022-07-22 21:58:09 +0530

    Praj-if elseif else

--- src/basic/IfElse.java
+++ src/basic/IfElse.java
@@ -4,12 +4,17 @@ public class IfElse {
     public static void main(String[] args) {
         int a = 20;
         int b = 30;
-        if (a>b) {
-            System.out.println("a gretest number");
+        int c = 40;
+        if (a>b && a>c) {
+            System.out.println("a greter number");
+        }
+        else if(b>c && b>a)
+        {
+            System.out.println("b is Greter number");
         }
         else
         {
-            System.out.println("b is Gretest number");
+            System.out.println("c is Greter number");
         }
     }
 }
