88b4f942cff1d22372b09eccb7d436d408416be0
uniqpraj86 <prajarote86@gmail.com>
2022-07-22 20:02:14 +0530

    Praj-Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter First no.");
+        int no1=s.nextInt();
+        System.out.println("Enter Second No");
+        int no2 = s.nextInt();
+        System.out.println("Select Symbol(+,-,*,/)");
+        String sym=s.next();
+        int res;
+        switch(sym)
+        {
+            case"+": res=no1+no2;
+                System.out.println("Addition is:"+res);
+                break;
+            case"-": res=no1-no2;
+                System.out.println("Substraction is:"+res);
+                break;
+            case"*": res=no1*no2;
+                System.out.println("Multiplication is:"+res);
+                break;
+            case"/": res=no1/no2;
+                System.out.println("Division is:"+res);
+                break;
+            default:
+                System.out.println("Invalid Symbol");
+                break;
+        }
+
+    }
+
+}
