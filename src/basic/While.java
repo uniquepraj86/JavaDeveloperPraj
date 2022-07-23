6885c065746c0053e5546d93855a9753ed61e865
uniqpraj86 <prajarote86@gmail.com>
2022-07-24 00:07:49 +0530

    Praj- while

---
+++ src/basic/While.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class While {
+    public static void main(String[] args) {
+        int no=54321,rem,rev=0;
+        while (no!=0)
+        {
+            rem=no%10;
+            rev=rev*10+rem;
+            no=no/10;
+        }
+        System.out.println(rev);
+    }
+}
