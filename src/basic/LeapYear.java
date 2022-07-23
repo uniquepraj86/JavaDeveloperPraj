9344effced1a3b983ec1878755af63f142e3abfb
uniqpraj86 <prajarote86@gmail.com>
2022-07-23 15:54:44 +0530

    Praj-3 Leap Year

---
+++ src/basic/LeapYear.java
@@ -0,0 +1,32 @@
+package basic;
+
+public class LeapYear
+{
+    public static void main(String[] args)
+    {
+       int year = 2020;
+       if(year%4==0)
+       {
+          if(year%100==0)
+          {
+              if(year%400==0)
+              {
+                  System.out.println("  leap year");
+               }
+              else
+              {
+                  System.out.println("not leap year");
+              }
+          }
+          else
+          {
+           System.out.println(" leap year");
+           }
+       }
+       else
+       {
+           System.out.println("not leap year");
+       }
+    }
+
+}
