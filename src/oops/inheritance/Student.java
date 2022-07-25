665a58ab0d64e2d3b0658bfb0509c8da37a3d5d8
uniqpraj86 <prajarote86@gmail.com>
2022-07-25 13:08:43 +0530

    Praj- Student percentage

---
+++ src/oops/inheritance/Student.java
@@ -0,0 +1,34 @@
+package oops.inheritance;
+
+public class Student {
+    String name="xyz";
+    int rno=1;
+    String div ="B";
+}
+class PhysicsMarks extends Student{
+    double marks = 89;
+}
+class ChemistryMarks extends Student{
+    double marks = 72;
+}
+class MathsMarks extends Student{
+    static double marks = 68;
+}
+
+
+class StudentImpl{
+    public static void main(String[] args) {
+        MathsMarks mathsMarks = new MathsMarks();
+        ChemistryMarks chemistryMarks = new ChemistryMarks();
+        PhysicsMarks physicsMarks = new PhysicsMarks();
+        double total= MathsMarks.marks+chemistryMarks.marks+physicsMarks.marks;
+        double percentages = (total/300)*100;
+        System.out.println(".........Student Details.......");
+        System.out.println("Roll No."+ mathsMarks.rno);
+        System.out.println("Name."+mathsMarks.name);
+        System.out.println("Div."+mathsMarks.div);
+        System.out.println("percentage."+percentages);
+        }
+
+    }
+
