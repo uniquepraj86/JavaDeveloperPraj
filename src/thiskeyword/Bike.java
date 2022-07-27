a1bf742d92882ae3df6275eeeee85a67f8903cc0
uniqpraj86 <prajarote86@gmail.com>
2022-07-27 23:34:32 +0530

    Praj-inheritance Aggrigation

---
+++ src/thiskeyword/Bike.java
@@ -0,0 +1,32 @@
+package thiskeyword;
+/**
+ * 3. used to invoke current class constructor
+ */
+public class Bike {
+    int bikeNo;
+    String bikeName;
+   public Bike()
+   {
+       this(101,"Activa");
+       System.out.println("in Default Constructor");
+   }
+
+    public Bike(int bikeNo, String bikeName)
+    {
+        System.out.println("in parametrised constructor");
+        this.bikeNo = bikeNo;
+        this.bikeName = bikeName;
+    }
+    void display()
+    {
+        System.out.println("Bike Details");
+        System.out.println("Bike No:"+bikeNo);
+        System.out.println("Bike Name;"+bikeName);
+    }
+}
+class BikeImpl{
+    public static void main(String[] args) {
+        Bike bike = new Bike();
+        bike.display();
+    }
+}
