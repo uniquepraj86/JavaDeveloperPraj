a47cdfa4e5f759d2015734f13664b54e32739f61
uniqpraj86 <prajarote86@gmail.com>
2022-07-27 22:14:57 +0530

    Praj-inheritance Aggrigation

---
+++ src/controlflowstmt/ForLoop.java
@@ -0,0 +1,26 @@
+package controlflowstmt;
+
+public class ForLoop {
+    public static void main(String args[]) {
+        //simple for loop
+        for (int i = 0; i < 3; i++) {
+            System.out.println(i);
+        }
+        //Nested for loop
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
+
+
+//        for(;;){
+//            System.out.println("Infinite Loop");
+//        }
+        //Enhanced for loop
+        int a[] = {5, 10, 15, 20, 25};
+        for (int no : a) {
+            System.out.println(no);
+        }
+    }
+}
