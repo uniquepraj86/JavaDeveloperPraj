50db9b847ec7a8d00f40a5ab7e2627f98f56ad49
uniqpraj86 <prajarote86@gmail.com>
2022-08-01 08:44:12 +0530

    Praj - Association

---
+++ src/finalkeyward/Person.java
@@ -0,0 +1,26 @@
+package finalkeyward;
+
+public class Person {
+    int empId;
+    String name;
+    final String PAN_NO = "AMP FT2453L";
+    final String AADHAAR_NO; //blank final variable
+    final String MOBILE_NO; //blank final variable
+    static final String BIRTH_DATE; //blank static final variable
+
+    static {
+        BIRTH_DATE = "15/09/1998";
+    }
+    Person(String aadhar_no, String mobileNo){
+        AADHAAR_NO = aadhar_no;
+        MOBILE_NO = mobileNo;
+    }
+
+}
+
+class PersonImpl{
+    public static void main(String[] args) {
+        Person person = new Person("789865324152","9865324152");
+    }
+
+}
