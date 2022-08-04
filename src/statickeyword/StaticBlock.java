2782c67216d39bf0f6116dae2ac1b30defe4dcb3
uniqpraj86 <prajarote86@gmail.com>
2022-08-04 14:26:54 +0530

    Praj - StaticKeyword

---
+++ src/statickeyword/StaticBlock.java
@@ -0,0 +1,10 @@
+package statickeyword;
+
+public class StaticBlock {
+    public static void main(String[] args) {
+        System.out.println("In main Method");
+    }
+    static {
+        System.out.println("In static block");
+    }
+}
