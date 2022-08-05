8120fab57b5da4978c8a427947a17a80ba1a513d
uniqpraj86 <prajarote86@gmail.com>
2022-08-05 23:05:50 +0530

    Praj - Exception Handling Nested try

---
+++ src/exceptionhandling/NestedTryDemo.java
@@ -0,0 +1,26 @@
+package exceptionhandling;
+
+import java.util.Locale;
+
+public class NestedTryDemo {
+    public static void main(String[] args) {
+        String msg = null;
+        try
+        {
+            System.out.println("1");
+            System.out.println("2");
+            System.out.println("3");
+            try{
+                System.out.println(10/0);
+            }catch (ArithmeticException e)
+            {
+                System.out.println(e.getMessage());
+            }
+            System.out.println(msg.toLowerCase());
+            System.out.println("Rest of the code 1");
+        }catch (NullPointerException e){
+        System.out.println(e.getMessage());
+        }
+        System.out.println("Rest of the code 2");
+    }
+}
