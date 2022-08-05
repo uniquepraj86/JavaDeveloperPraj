9fac1b4b4d5a5f38c1deaae606676028c6c3f725
uniqpraj86 <prajarote86@gmail.com>
2022-08-05 08:54:53 +0530

    Praj - Exception Handling Introduction

---
+++ src/exceptionhandling/NullPointerExecptionDemo.java
@@ -0,0 +1,13 @@
+package exceptionhandling;
+
+import java.util.Locale;
+
+public class NullPointerExecptionDemo {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        System.out.println(a[6]);
+        String name = null;
+        System.out.println(name.toLowerCase());
+    }
+
+}
