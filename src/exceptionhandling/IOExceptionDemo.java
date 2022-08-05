324e08d0bad8e3b6b6026d42f7f6b76211ea1aa0
uniqpraj86 <prajarote86@gmail.com>
2022-08-05 08:57:14 +0530

    Praj - Exception Handling Introduction

---
+++ src/exceptionhandling/IOExceptionDemo.java
@@ -0,0 +1,11 @@
+package exceptionhandling;
+
+import java.io.File;
+
+public class IOExceptionDemo{
+    public static void main(String[] args) {
+        File file = new File("abc.txt");
+        //IOException
+        //  file.createNewFile(); //checked/compile exception
+    }
+}
