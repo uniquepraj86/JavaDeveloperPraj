949cb81a2136c14f1c08958ed9a318b3d0b8a544
uniqpraj86 <prajarote86@gmail.com>
2022-08-06 10:55:31 +0530

    Praj - Exception Handling Finally

---
+++ src/exceptionhandling/FinallyDemo.java
@@ -0,0 +1,16 @@
+package exceptionhandling;
+
+public class FinallyDemo {
+    public static void main(String[] args) {
+        String msg = null;
+        try {
+            System.out.println(msg.length());
+        }catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        }finally {
+            System.out.println("In finally block");
+        }
+        System.out.println("Rest of the code");
+        }
+    }
+
