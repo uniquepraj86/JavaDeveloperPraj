ac5a1dc918f7b760284bc17cbb34d7bee43f65e2
uniqpraj86 <prajarote86@gmail.com>
2022-08-13 23:49:35 +0530

    Praj-JDBC Delete and Update;

---
+++ src/jdbc/MySqlUpdateDemo.java
@@ -0,0 +1,24 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class MySqlUpdateDemo {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String url = "jdbc:mysql://localhost:3306/emp";
+        String username = "root";
+        String password = "Prajkta86@";
+        final String QUERY = "update emp set job = ? where empno = ?";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url,username,password);
+        PreparedStatement ps = con.prepareStatement(QUERY);
+        ps.setString(1,"SALESMAN");
+        ps.setInt(2,121);
+        int no = ps.executeUpdate();
+        System.out.println(no +"row/s affected");
+
+    }
+}
