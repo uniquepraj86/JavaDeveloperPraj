8fd0b9f42dfd15bf2541cd9e85c32af6d7007141
uniqpraj86 <prajarote86@gmail.com>
2022-08-17 16:27:40 +0530

    Praj-String

---
+++ src/pracrice/Marks.java
@@ -0,0 +1,39 @@
+package pracrice;
+
+import java.util.Scanner;
+
+public class Marks {  public static void main(String[] args) {
+    int mark;
+    Scanner scanner=new Scanner(System.in);
+    System.out.println("*********** Enter Marks **********");
+
+    mark=scanner.nextInt();
+    if (mark>=90&&mark<=100) {
+        System.out.println("O");
+    }
+    else if (mark>=80&&mark<90) {
+        System.out.println("A+");
+    }
+    else if (mark>=70&&mark<80) {
+        System.out.println("A");
+    }
+    else if (mark>=60&&mark<70) {
+        System.out.println("B+");
+    }
+    else if (mark>=50&&mark<60) {
+        System.out.println("C+");
+    }
+    else if (mark>=40&&mark<50) {
+        System.out.println("C");
+    }
+    else if (mark>=35&&mark<40) {
+        System.out.println("D");
+    }
+    else if (mark<35) {
+        System.out.println("Fail");
+    }
+    else {
+        System.out.println("Not Correct");
+    }
+}
+}
---
+++ src/string/StringExample.java
@@ -0,0 +1,17 @@
+package string;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String str = "my name is prajakta arote";
+        System.out.println("Upper Case: "+str.toUpperCase());
+        System.out.println("Lower Case: "+str.toLowerCase());
+        System.out.println("Char At: "+str.charAt(3));
+        System.out.println("Concat: "+str.concat(" Pvt. Ltd."));
+        System.out.println("Replace: "+str.replace("in","at"));
+        System.out.println("Replace All "+str.replaceAll("in","at"));
+        System.out.println("Contains "+str.contains("brings"));
+        System.out.println(str);
+
+
+    }
+}
