2d1f42c1682dccbb031f6acb7720074e7cc24d4b
uniqpraj86 <prajarote86@gmail.com>
2022-08-24 17:20:18 +0530

    Praj-interface list (collection)

---
+++ src/colletion/ArrayListDemo.java
@@ -0,0 +1,26 @@
+package colletion;
+import java.util.ArrayList;
+import java.util.List;
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Pune");
+        list.add("pune");
+        list.add("Chennai");
+        list.add("Kolkata");
+        list.add("Madras");
+        System.out.println(list);
+        System.out.println(list.size());
+        list.add(3,"Bangalore");
+        System.out.println(list);
+        if (!list.isEmpty()){
+
+        }
+        System.out.println(list.get(2));
+        System.out.println(list.contains("Madras1"));
+        System.out.println(list.indexOf("Bangalore"));
+        //list.containsAll()
+        list.remove("Kolkata");
+        System.out.println(list);
+    }
+}
---
+++ src/colletion/VectorDemo.java
@@ -0,0 +1,18 @@
+package colletion;
+
+import java.util.Vector;
+
+/*
+
+ */
+
+
+public class VectorDemo {
+    public static void main(String[] args) {
+        Vector<Integer> vector= new Vector<>();
+        vector.add(1);
+        vector.add(2);
+        vector.add(3);
+        System.out.println(vector);
+    }
+}
