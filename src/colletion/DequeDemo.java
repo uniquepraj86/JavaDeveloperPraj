851ca56994f3523dae79ca416010f21ea47c463a
uniqpraj86 <prajarote86@gmail.com>
2022-08-30 18:08:22 +0530

    Praj - Array,Queue,Deque

---
+++ src/colletion/DequeDemo.java
@@ -0,0 +1,23 @@
+package colletion;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DequeDemo {
+    public static void main(String[] args) {
+        // Deque<String> cities = new LinkedList<>();
+        Deque<String> cities = new ArrayDeque<>();
+        cities.addFirst("Pune");
+        cities.addFirst("Mumbai");
+        cities.offerFirst("Ooty");
+        cities.offerLast("Nagpur");
+        cities.addLast("Kolkata");
+        System.out.println(cities);
+        System.out.println(cities.peekFirst());
+        System.out.println(cities.peekLast());
+        System.out.println(cities.poll());
+        System.out.println(cities);
+
+    }
+
+}
