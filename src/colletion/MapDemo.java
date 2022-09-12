b89c2477d4c865238c949bac5ad01c67ce9d8974
uniqpraj86 <prajarote86@gmail.com>
2022-09-12 17:22:23 +0530

    Praj -collection FrameWork

---
+++ src/colletion/MapDemo.java
@@ -0,0 +1,30 @@
+package colletion;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class MapDemo {
+    public static void main(String[] args) {
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1,"praj");
+        map.put(2,"sneha");
+        map.put(3,"ruth");
+        map.put(4,"pranali");
+        System.out.println(map);
+        map.put(null,null);
+        System.out.println(map);
+        map.put(1,null);
+        System.out.println(map);
+        map.put(null,"praj");
+        System.out.println(map);
+        System.out.println(map.get(5));
+        Iterator iterator = map.entrySet().iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        map.remove(5);
+        System.out.println(map);
+    }
+
+}
