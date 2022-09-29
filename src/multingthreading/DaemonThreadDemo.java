37599f9689d90edd23ab669bf768d306450e697e
uniqpraj86 <prajarote86@gmail.com>
2022-09-29 09:28:31 +0530

    praj - Thread,Yield

---
+++ src/multingthreading/DaemonThreadDemo.java
@@ -0,0 +1,15 @@
+package multingthreading;
+
+public class DaemonThreadDemo extends Thread{
+    @Override
+    public void run() {
+        System.out.println("run Method");
+    }
+
+    public static void main(String[] args) {
+        DaemonThreadDemo t1 = new DaemonThreadDemo();
+        t1.setDaemon(true);
+        t1.start();
+        System.out.println(t1.isDaemon());
+    }
+}
