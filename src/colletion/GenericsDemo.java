1e175fba7f493acfb8424993129aa1c8274dd6f3
prajkta <prajktaarote86@gmail.com>
2022-10-11 18:29:52 +0530

    praj -test

--- src/colletion/GenericsDemo.java
+++ src/colletion/GenericsDemo.java
@@ -1,17 +1,17 @@
-package colletion;
-import java.util.List;
-public class GenericsDemo <E> {
-        E t;
-        E getValue(E t) {
-            this.t = t;
-            return t;
-        }
-
-        public static void main(String[] args) {
-            GenericsDemo demo = new GenericsDemo();
-            System.out.println(demo.getValue(5));
-            System.out.println(demo.getValue("Hi"));
-            System.out.println(demo.getValue(true));
-        }
-    }
-
+//package colletion;
+//import java.util.List;
+//public class GenericsDemo <E> {
+//        E t;
+//        E getValue(E t) {
+//            this.t = t;
+//            return t;
+//        }
+//
+//        public static void main(String[] args) {
+//            GenericsDemo demo = new GenericsDemo();
+//            System.out.println(demo.getValue(5));
+//            System.out.println(demo.getValue("Hi"));
+//            System.out.println(demo.getValue(true));
+//        }
+//    }
+//
--- src/interfacedemo/Bank.java
+++ src/interfacedemo/Bank.java
@@ -28,3 +28,4 @@ class BankImpl{
         bank.calculateInterest();
     }
 }
+
