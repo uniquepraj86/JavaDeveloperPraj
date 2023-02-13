20993879f0fb420d4585267cd5144f65cd8ec3aa
prajkta <prajktaarote86@gmail.com>
2023-02-13 09:50:10 +0530

    praj-basic

---
+++ src/assignments/EvenOdd.java
@@ -0,0 +1,21 @@
+package assignments;
+
+import java.util.Scanner;
+class EvenOdd
+{
+    public static void main(String[] args)
+    {
+        int no;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter any number :");
+        no=s.nextInt();
+        if(no%2==0)
+        {
+            System.out.println("Even number");
+        }
+        else
+        {
+            System.out.println("Odd number");
+        }
+    }
+}
--- src/assignments/starspattern/Star1.java
+++ src/assignments/starspattern/Star1.java
@@ -4,14 +4,16 @@ public class Star1
 {
     public static void main(String[] args)
     {
-        for( int i=3;i>0;i--)
-        {
-            for(int j=3;j<=3;j--)
-              {
-                System.out.println(" * ");
-              }
-            System.out.println(" ");
-        }
+//        for( int i=3;i>0;i--)
+//        {
+//            for(int j=3;j<=i;j--)
+//              {
+//                System.out.println(" * ");
+//              }
+//            System.out.println(" ");
+//        }
+
+
     }
 }
 
